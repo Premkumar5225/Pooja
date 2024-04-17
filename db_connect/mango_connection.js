22ac528668237b9b8b5c5ef70c34946dce7353b7
Priyatham Kotha <priyathamkotha@Priyathams-MacBook-Pro.local>
2024-04-17 19:49:44 -0400

    first commit

--- db_connect/mango_connection.js
+++ db_connect/mango_connection.js
@@ -1,6 +1,6 @@
 module.exports = {
     connectionurl: 'mongodb+srv://priyatham52:Mm1yKYGTQVkTuTF3@cluster0.yh7n8gh.mongodb.net/',
-    database: 'NewsFeed',
-    collection: 'News'
+    database: 'tickets',
+    collection: 'ticket_details'
   };
   
